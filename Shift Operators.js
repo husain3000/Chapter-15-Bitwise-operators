a78bf2795d894816d8ae72fd1b6eaf47e3f4959c
husain3000 <73273019+husain3000@users.noreply.github.com>
2023-02-26 12:35:54 +0530

    first commit

---
+++ Shift Operators.js
@@ -0,0 +1,3 @@
+console.log(5 << 2);    // Left shift
+
+console.log(20 >> 2);   //Right shift
---
+++ bitwise operators.js
@@ -0,0 +1,9 @@
+console.log(1|2);  //OR operator
+
+
+console.log(1 & 2);  //AND operator
+
+
+console.log(13 ^ 7);   //XOR
+
+console.log(~13);       // NOT
